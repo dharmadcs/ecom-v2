4fd79debfb09fd7d499650f5a01db7a5b181ca72
dharmadcs <dharmajiwork@gmail.com>
2022-08-08 23:41:05 +0700

    initial commit

---
+++ resources/views/livewire/contact.blade.php
@@ -0,0 +1,28 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <br>
+            <img src="/assets/dharma.png" align="left" width="200" class="img-thumbnail rounded-circle">
+            <h1> &nbsp;Dharma Ajie</h1>
+            <h3> &nbsp; Backend Developer</h3>
+            <h4> &nbsp; 0823-1077-4281</h4>
+            <h5> &nbsp; dharmajiwork@gmail.com</h5>
+            <br>
+            <br>
+            <br>
+            <h5> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                This is one of my portfolio websites, for now I'm looking for a job as a web developer.
+                A student at STMIK Jayakarta, has
+                interests and abilities
+                in making a website
+                application,
+                especially on the backend. My initial
+                programming language is PHP.
+                Desire to master other backend
+                programming languages.
+            </h5>
+            <br>
+
+        </div>
+    </div>
+</div>
